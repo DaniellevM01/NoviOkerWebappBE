799899a8e67030774334e49a549ce2b912393b17
DaniellevM01 <daniellevmanen@gmail.com>
2022-01-19 17:55:15 +0100

    Fixed mysterious namegiving for dto's and added the new patch ones for offerapplication and vacancyapplication

--- src/main/java/nl/novi/okerwebapp/dto/requests/VacancyApplicationPatchRequestDto.java
+++ src/main/java/nl/novi/okerwebapp/dto/requests/VacancyApplicationPatchRequestDto.java
@@ -1,4 +1,17 @@
 package nl.novi.okerwebapp.dto.requests;
 
+import javax.validation.constraints.NotBlank;
+
 public class VacancyApplicationPatchRequestDto {
+
+    @NotBlank
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
