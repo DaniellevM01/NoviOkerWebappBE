8bec73eaaa81c66231faae35b9526afd1c3281f9
DaniellevM01 <daniellevmanen@gmail.com>
2022-01-25 19:43:54 +0100

    removal of functions and transports

--- src/main/java/nl/novi/okerwebapp/repository/FunctionRepository.java
+++
@@ -1,8 +0,0 @@
-package nl.novi.okerwebapp.repository;
-
-import nl.novi.okerwebapp.model.Function;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FunctionRepository extends CrudRepository<Function, Integer> {
-
-}
